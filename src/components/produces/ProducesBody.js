4c7c0a63f71e9be11a13d8c0bb45408d416242c8
Cleydyr Albuquerque <cleydyr.albuquerque@liferay.com>
2019-05-22 00:17:55 -0300

    WIP Animated button

--- src/components/produces/ProducesBody.js
+++ src/components/produces/ProducesBody.js
@@ -5,9 +5,11 @@ import {
 	Text,
 	FlatList,
 	SectionList,
+	TouchableOpacity,
+	Animated,
 } from 'react-native';
 
-import { GREEN_MAIN } from '../../util/Colors';
+import { GREEN_MAIN, BUTTON_MAIN_ACTIVE } from '../../util/Colors';
 
 import ProducesListBanner from './ProducesListBanner';
 import ProducesListItem from './ProducesListItem';
@@ -69,16 +71,87 @@ const productData = [
 	},
 ];
 
+const ANIMATION_DURATION_COLLAPSE = 200;
+const ANIMATION_DURATION_EXPAND = 250;
+const SEND_LIST_HEIGHT = 58;
+
 export default class ProducesBody extends React.Component {
+	state = {
+		sendListAnim: new Animated.Value(0),
+		animating: false,
+	}
+
+	handleSendListVisibility = (e) => {
+		const delta = e.nativeEvent.velocity.y;
+		const {animating} = this.state;
+
+		if (delta >= 0 && !animating) {
+			this.setState({
+				animating: true,
+			});
+
+			Animated.timing(
+				this.state.sendListAnim,
+				{
+					toValue: SEND_LIST_HEIGHT,
+					duration: ANIMATION_DURATION_COLLAPSE,
+					useNativeDriver: true,
+				}
+			).start(() => {
+				this.setState({
+					animating: false,
+				});
+			});
+		}
+		else if (!animating) {
+			this.setState({
+				animating: true,
+			});
+
+			Animated.timing(
+				this.state.sendListAnim,
+				{
+					toValue: 0,
+					duration: ANIMATION_DURATION_EXPAND,
+					useNativeDriver: true,
+				}
+			).start(() => {
+				this.setState({
+					animating: false,
+				});
+			});
+		}
+	}
+
 	render() {
+		const {sendListAnim} = this.state;
+
 		return (
 			<View style={styles.container}>
 				<SectionList
+					scrollEventThrottle={50}
+					onScroll={this.handleSendListVisibility}
 					sections={[{data: productData}]}
 					renderSectionHeader={() => <ProducesListBanner submissionDeadline="20/08/2019" produceQuantity={120}/>}
 					renderItem={({item}) => <ProducesListItem {...item}/>}
 					keyExtractor={(item, index) => index}
 				/>
+				<Animated.View style={
+					[
+						styles.animatedView,
+						{
+							transform: [
+								{
+									translateY: sendListAnim,
+								}
+							],
+						}
+					]
+				}>
+					<TouchableOpacity style={styles.sendListButton}>
+						<Text style={styles.sendListText}>Enviar lista para clientes</Text>
+					</TouchableOpacity>
+				</Animated.View>
 			</View>
 		);
 	}
@@ -88,4 +161,22 @@ const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 	},
+	sendListButton: {
+		backgroundColor: BUTTON_MAIN_ACTIVE,
+		height: 58,
+		width: 360,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	sendListText: {
+		color: 'white',
+		lineHeight: 30,
+		fontFamily: 'Roboto',
+		fontWeight: 'bold',
+		fontSize: 19,
+	},
+	animatedView: 	{
+		position: 'absolute',
+		bottom: 0,
+	}
 });
