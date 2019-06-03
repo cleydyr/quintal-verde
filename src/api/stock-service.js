b539e480bbc50461f4bb1024226ffc481ff5fa4e
Cleydyr Albuquerque <cleydyr.albuquerque@liferay.com>
2019-06-03 19:41:36 -0300

    Insert produce availability on data

--- src/api/stock-service.js
+++ src/api/stock-service.js
@@ -1,17 +1,48 @@
 import { stockItems } from './stock-items.json';
 import { produces } from './produces.json';
+import { stocks } from './stocks.json';
+import { users } from './users.json';
 
 export async function prepareStocks(expirationDate) {
-	return stockItems.map(({produceId, quantity}) => ({
-		...produces[produceId],
-		quantity,
-	})).reduce((acc, cur) => {
-		const {id, quantity} = cur;
+	const data = stockItems.map(({produceId, quantity, stockId}) => {
+		const user = users.find(user => user.login.uuid === stocks.find(stock => stock.stockId === stockId).userId);
+		const userName = `${user.name.first} ${user.name.last}`;
+		const userId = user.login.uuid;
 
-		return acc.find(item => item.id === id)
-			? acc.map(item => item.id === id ? {...item, quantity: item.quantity + quantity} : item)
-			: [...acc, cur];
+		return {
+			...produces[produceId],
+			quantity,
+			stockId,
+			availability: [
+				{
+					userId: userId,
+					name: userName,
+					quantity,
+				},
+			],
+		};
+	}).reduce((acc, cur) => {
+		const {id, quantity, stockId, availability} = cur;
+
+		if (acc.some(item => item.id === id)) {
+			return acc.map(item =>
+				item.id === id
+				? {
+						...item,
+						quantity: item.quantity + quantity,
+						availability: [
+							...item.availability,
+							...availability,
+						]
+					}
+				: item);
+		}
+		else {
+			return [...acc, cur];
+		}
 	}, []);
+
+	return new Promise(resolve => setTimeout(() => resolve(data), 1500));
 }
 
 export async function getStocks() {
