363ac07d63a16ab7d306e45364b8d19d93e296c9
Cleydyr Albuquerque <cleydyr.albuquerque@liferay.com>
2019-06-05 00:52:58 -0300

    Move it even upper

--- src/components/produces/list/ProducesBody.js
+++ src/components/produces/list/ProducesBody.js
@@ -8,35 +8,8 @@ import {
 
 import ProducesListBanner from './ProducesListBanner';
 import ProducesListItem from './ProducesListItem';
-import { prepareStocks } from '../../../api/stock-service';
-import LoadingScreen from '../../LoadingScreen';
-import FloatingScrollAwareButton from './FloatingScrollAwareButton';
-
 
 export default class ProducesBody extends React.Component {
-	state = {
-		loading: true,
-		y: 0,
-		delta: 0,
-	}
-
-	handleSendListVisibility = e => {
-		const delta = e.nativeEvent.velocity.y;
-		const y = e.nativeEvent.contentOffset.y; // To avoid hiding on bounce at top
-		this.setState({
-			y,
-			delta,
-		});
-	}
-
-	componentDidMount = async () => {
-		const productData = await prepareStocks();
-		this.setState({
-			productData,
-			loading: false,
-		});
-	}
-
 	navigateToDetails = item => {
 		return () => this.props.navigation.navigate('ProduceDetails', {
 			item,
@@ -44,26 +17,18 @@ export default class ProducesBody extends React.Component {
 	}
 
 	render() {
-		const {sendListAnim, productData, loading} = this.state;
-		const {navigation} = this.props;
-
-		if (loading) {
-			return (
-				<LoadingScreen text="Carregando itens. Aguarde." />
-			);
-		}
+		const {productData, onScroll} = this.props;
 
 		return (
 			<View style={styles.container}>
 				<SectionList
 					scrollEventThrottle={50}
-					onScroll={this.handleSendListVisibility}
+					onScroll={onScroll}
 					sections={[{data: productData || []}]}
 					renderSectionHeader={() => <ProducesListBanner submissionDeadline="20/08/2019" produceQuantity={productData && productData.length || 0}/>}
 					renderItem={({item}) => <ProducesListItem onPress={this.navigateToDetails(item)} {...item}/>}
 					keyExtractor={(item, index) => index}
 				/>
-				<FloatingScrollAwareButton buttonText="Enviar lista para clientes" y={y} delta={delta} />
 			</View>
 		);
 	}
--- src/components/produces/list/ProducesScreen.js
+++ src/components/produces/list/ProducesScreen.js
@@ -4,8 +4,13 @@ import {
 	View,
 } from 'react-native';
 
+import { prepareStocks } from '../../../api/stock-service';
+
 import ProducesHeader from './ProducesHeader';
 import ProducesBody from './ProducesBody';
+import LoadingScreen from '../../LoadingScreen';
+import FloatingScrollAwareButton from './FloatingScrollAwareButton';
+
 import { GREEN_MAIN } from '../../../util/Colors';
 
 export default class ProducesScreen extends React.Component {
@@ -18,12 +23,44 @@ export default class ProducesScreen extends React.Component {
 		headerTintColor: 'white',
 	};
 
+	state = {
+		y: 0,
+		delta: 0,
+		loading: true,
+	}
+
+	componentDidMount = async () => {
+		const productData = await prepareStocks();
+		this.setState({
+			productData,
+			loading: false,
+		});
+	}
+
+	handleSendListVisibility = e => {
+		const delta = e.nativeEvent.velocity.y;
+		const y = e.nativeEvent.contentOffset.y; // To avoid hiding on bounce at top
+
+		this.setState({
+			y,
+			delta,
+		});
+	}
+
 	render() {
 		const {navigation} = this.props;
+		const {y, delta, loading, productData} = this.state;
+
+		if (loading) {
+			return (
+				<LoadingScreen text="Carregando itens. Aguarde." />
+			);
+		}
 
 		return (
 			<View style={styles.container}>
-				<ProducesBody navigation={navigation}/>
+				<ProducesBody productData={productData} onScroll={this.handleSendListVisibility} navigation={navigation}/>
+				<FloatingScrollAwareButton buttonText="Enviar lista para clientes" y={y} delta={delta} />
 			</View>
 		);
 	}
